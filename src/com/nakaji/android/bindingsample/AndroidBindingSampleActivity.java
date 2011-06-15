005912bd366495e684a95d96e4efb5f7a87d0e13
nakaji <nakaji126@gmail.com>
2011-06-15 23:37:33 +0900

    PojoViewModelを使用する様に変更
    2011/06/16のエントリ
    http://d.hatena.ne.jp/nakaji999/20110615/1308152973

--- src/com/nakaji/android/bindingsample/AndroidBindingSampleActivity.java
+++ src/com/nakaji/android/bindingsample/AndroidBindingSampleActivity.java
@@ -3,26 +3,44 @@ package com.nakaji.android.bindingsample;
 import gueei.binding.Binder;
 import gueei.binding.Command;
 import gueei.binding.observables.StringObservable;
+import gueei.binding.pojo.PojoViewModel;
+import gueei.binding.pojo.PojoViewModelHelper;
+import gueei.binding.pojo.PojoViewModelWrapper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class AndroidBindingSampleActivity extends Activity {
+public class AndroidBindingSampleActivity extends Activity implements PojoViewModel {
+
+    private PojoViewModelHelper helper = new PojoViewModelHelper();
+
+    public PojoViewModelHelper getHelper() {
+        return helper;
+    }
+
+    public void notifyPropertyChanged(String propertyName) {
+        helper.notifyPropertyChanged(propertyName);
+    }
 
     private EditText heightText;
     private EditText weightText;
+    private double _bmi;
 
-    public StringObservable BMI = new StringObservable("");
+    public void setBMI(double bmi) {
+        _bmi = bmi;
+        notifyPropertyChanged("BMI");
+    }
 
-    public Command Calculate = new Command() {
-        @Override
-        public void Invoke(View arg0, Object... arg1) {
-            double height = Integer.valueOf(heightText.getText().toString());
-            double weight = Integer.valueOf(weightText.getText().toString());
-            double bmi = weight / Math.pow(height / 100.0, 2);
-            BMI.set("Your BMI is " + String.valueOf(bmi));
-        }
+    public String getBMI() {
+        return "Your BMI is " + String.valueOf(_bmi);
+    }
+
+    public void Calculate() {
+        double height = Integer.valueOf(heightText.getText().toString());
+        double weight = Integer.valueOf(weightText.getText().toString());
+        double bmi = weight / Math.pow(height / 100.0, 2);
+        setBMI(bmi);
     };
 
     /** Called when the activity is first created. */
@@ -31,10 +49,9 @@ public class AndroidBindingSampleActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         Binder.init(this.getApplication());
-        Binder.setAndBindContentView(this, R.layout.main, this);
+        Binder.setAndBindContentView(this, R.layout.main, PojoViewModelWrapper.create(this));
 
         heightText = (EditText) findViewById(R.id.height);
         weightText = (EditText) findViewById(R.id.weight);
     }
-
 }
