f206f61a5b35fb3ef9d8ccaaeb025fa07f1cb64e
nakaji <nakaji126@gmail.com>
2012-02-19 17:18:59 +0900

    Validationを追加

--- BMICalc/src/com/nakaji/android/bmicalc/BMIViewModel.java
+++ BMICalc/src/com/nakaji/android/bmicalc/BMIViewModel.java
@@ -2,6 +2,9 @@ package com.nakaji.android.bmicalc;
 
 import gueei.binding.Command;
 import gueei.binding.observables.StringObservable;
+import gueei.binding.validation.ModelValidator;
+import gueei.binding.validation.ValidationResult;
+import gueei.binding.validation.validators.Required;
 
 import java.math.BigDecimal;
 
@@ -9,17 +12,28 @@ import android.view.View;
 
 public class BMIViewModel {
     public StringObservable BMI = new StringObservable();
+    @Required(ErrorMessage = "Height is required.")
     public StringObservable Height = new StringObservable();
+    @Required(ErrorMessage = "Weight is required.")
     public StringObservable Weight = new StringObservable();
 
     public Command Calculate = new Command() {
         @Override
         public void Invoke(View arg0, Object... arg1) {
-            double height = Double.valueOf(Height.get());
-            double weight = Double.valueOf(Weight.get());
-            double bmi = weight / Math.pow(height / 100.0, 2);
-            BigDecimal bd = new BigDecimal(bmi);
-            BMI.set(bd.setScale(2, BigDecimal.ROUND_HALF_UP).toString());
+            ValidationResult res = ModelValidator.ValidateModel(BMIViewModel.this);
+            if (res.isValid()) {
+                double height = Double.valueOf(Height.get());
+                double weight = Double.valueOf(Weight.get());
+                double bmi = weight / Math.pow(height / 100.0, 2);
+                BigDecimal bd = new BigDecimal(bmi);
+                BMI.set(bd.setScale(2, BigDecimal.ROUND_HALF_UP).toString());
+            } else {
+                String errorMessage = "";
+                for (String error : res.getValidationErrors()) {
+                    errorMessage += error + "\n";
+                }
+                BMI.set(errorMessage);
+            }
         }
     };
 }
